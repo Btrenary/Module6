a72db664c6c9ce103c1050d7f1a41f17f9ae8ce6
Btrenary <btrenary25@gmail.com>
2020-10-06 21:53:57 -0500

    program successful

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -1,5 +1,20 @@
 """
+Author: Brady Trenary
 
-
+Program accepts string and number, then multiplies string by the number.
 """
 
+
+def multiply_string(message, n):
+    """
+    :param message: string
+    :param n: int
+    :return: string
+    """
+    result = message * n
+    print(result)
+    return result
+
+
+if __name__ == '__main__':
+    multiply_string('Brady', 5)
