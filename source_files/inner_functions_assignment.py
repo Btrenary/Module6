48dba4b33b5b6f167b6c3b282c9f73fc66c3efbf
Btrenary <btrenary25@gmail.com>
2020-10-06 22:26:21 -0500

    test pass

--- source_files/inner_functions_assignment.py
+++ source_files/inner_functions_assignment.py
@@ -10,15 +10,31 @@ def measurements(rect_measure):
     p_value = 0.0
 
     def area(a_measure):
-        return a_measure[0] * a_measure[1]
+        if len(a_measure) == 1:
+            return a_measure[0] * a_measure[0]
+        elif len(a_measure) == 2:
+            return a_measure[0] * a_measure[1]
+        else:
+            raise IndexError
 
     def perimeter(p_measure):
-        return p_measure[0] * 2 + p_measure[1] * 2
+        if len(p_measure) == 1:
+            return p_measure[0] * 4
+        elif len(p_measure) == 2:
+            return p_measure[0] * 2 + p_measure[1] * 2
+        else:
+            raise IndexError
 
-    a_value = area(rect_measure)
-    p_value = perimeter(rect_measure)
-    return "Perimeter = {} Area = {}".format(p_value, a_value)
+    try:
+        a_value = area(rect_measure)
+        p_value = perimeter(rect_measure)
+    except IndexError:
+        raise IndexError
+    return f"Perimeter = {p_value} Area = {a_value}"
 
 
 if __name__ == '__main__':
-    print(measurements([2, 3]))
+    try:
+        print(measurements([2, 3]))
+    except IndentationError:
+        print("Enter 1 or 2 measurements for area and perimeter.")
